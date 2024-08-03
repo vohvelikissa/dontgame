f321bfc7eeedff446389dff709cccb6917c644a3
Pessi <--add>
2024-08-03 15:14:10 +0300

    basic loopdy loop :D

--- game.js
+++ game.js
@@ -1,21 +1,67 @@
-// Create a Pixi Application
-const app = new PIXI.Application({
-	width: window.innerWidth,   // width of the canvas
-	height: window.innerHeight, // height of the canvas
-	backgroundColor: 0xFFFFFF,  // background color of the canvas
-});
-
-// Add the Pixi canvas to the HTML document
-document.body.appendChild(app.view);
-
-//TODO: main menu
-//TODO: decision screen
-//TODO: moving in map
-//TODO: the good or bad things moving in map
-//TODO: judgement screen
-//TODO: credits screen
-
-// Resize the Pixi application when the window is resized
-window.addEventListener('resize', () => {
-	app.renderer.resize(window.innerWidth, window.innerHeight);
-});
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+var stateOfTheGame = 0;
+var timeUntilGameStarts = 100;
+var backupTime = 100;
+//TODO: Main menu
+function mainMenuScreen() {
+	ctx.fillStyle = "#000000";
+	console.log("you are supposed to be in main menu");
+	ctx.font = "20px Georgia";
+	ctx.fillText("Don't, the game", 10, 50);
+	ctx.fillText("Your game starts in "+timeUntilGameStarts, 10, 80);
+	timeUntilGameStarts--;
+	if(timeUntilGameStarts<=0) {
+		stateOfTheGame = 2;
+	}
+}
+//TODO: Judgement screen
+function judgementScreen() {
+	ctx.fillStyle = "#000000";
+	console.log("you are supposed to be in judgement screen");
+	ctx.font = "20px Georgia";
+	ctx.fillText("Be judged. You probably were no good.", 10, 50);
+	timeUntilGameStarts--;
+	if(timeUntilGameStarts<=0) {
+		stateOfTheGame = 3;
+	}
+}
+//TODO: Normal movement
+function normalMovementScreen() {
+	ctx.fillStyle = "#000000";
+	console.log("you are supposed to be in normal movement");
+	timeUntilGameStarts = backupTime;
+	stateOfTheGame = 1;
+}
+//TODO: Credit screen
+function creditScreen() {
+	ctx.fillStyle = "#000000";
+	console.log("you are supposed to be in credits screen");
+	ctx.font = "20px Georgia";
+	ctx.fillText("Game over",10,50);
+	timeUntilGameStarts = backupTime;
+	stateOfTheGame = 0;
+}
+//TODO: screen selector
+function screenSelector() {
+	if(stateOfTheGame == 0) {
+		mainMenuScreen();
+	} else if (stateOfTheGame == 1) {
+		judgementScreen();
+	} else if (stateOfTheGame == 2) {
+		normalMovementScreen();
+	} else if (stateOfTheGame == 3) {
+		creditScreen();
+	}
+}
+function clearScreen() {
+	ctx.fillStyle = "#ffffff";
+	ctx.fillRect(0,0,999,999);
+}
+function gameLoop() {
+	clearScreen();
+	screenSelector();
+	console.log("loopdy loop :D");
+	requestAnimationFrame(gameLoop);
+}
+gameLoop();
